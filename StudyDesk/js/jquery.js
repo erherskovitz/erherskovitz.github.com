557e01966276b9e97ff882bab41e8e26ea77d9ed
Libby Herskovitz <erherskovitz@hotmail.com>
2013-03-30 22:31:25 -0400

    JQUERY YES!

--- StudyDesk/js/jquery.js
+++ StudyDesk/js/jquery.js
@@ -1,6 +1,5 @@
 
   $("a").click( function () {
-  	console.log('This message will be logged, but will not cause an error in IE7');
 	$(this).addClass(".info");},
   function () {
     $(this).removeClass(".info");
